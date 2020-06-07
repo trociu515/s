77623ebe5059a4698e5474ed6a3d78a298c84f59
RobertKrzywina <robert.krzywina@gmail.com>
2020-06-07 19:45:18 +0200

    Add translation for employee details page

--- lib/employee/employee_details.dart
+++ lib/employee/employee_details.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:give_job/employee/employee_service.dart';
 import 'package:give_job/employee/employee_side_bar.dart';
+import 'package:give_job/internationalization/localization/localization_constants.dart';
 import 'package:give_job/shared/app_bar.dart';
 import 'package:give_job/shared/constants.dart';
 
@@ -52,97 +53,150 @@ class _EmployeeDetailsState extends State<EmployeeDetails> {
                           crossAxisAlignment: CrossAxisAlignment.center,
                           children: <Widget>[
                             ListTile(
-                              title: Text('Identyfikator'),
-                              subtitle: Text(employee.id.toString()),
+                              title: Text(getTranslated(context, 'id')),
+                              subtitle: Text(employee.id != null
+                                  ? employee.id.toString()
+                                  : getTranslated(context, 'empty')),
                             ),
                             ListTile(
-                              title: Text('Nazwa użytkownika'),
-                              subtitle: Text(utf8
-                                  .decode(employee.username.runes.toList())),
-                            ),
-                            ListTile(
-                              title: Text('Imię'),
+                              title: Text(getTranslated(context, 'username')),
                               subtitle: Text(
-                                  utf8.decode(employee.name.runes.toList())),
+                                utf8.decode(employee.username != null
+                                    ? employee.username.runes.toList()
+                                    : getTranslated(context, 'empty')),
+                              ),
                             ),
                             ListTile(
-                              title: Text('Nazwisko'),
+                              title: Text(getTranslated(context, 'name')),
+                              subtitle: Text(utf8.decode(employee.name != null
+                                  ? employee.name.runes.toList()
+                                  : getTranslated(context, 'empty'))),
+                            ),
+                            ListTile(
+                              title: Text(getTranslated(context, 'surname')),
+                              subtitle: Text(utf8.decode(
+                                  employee.surname != null
+                                      ? employee.surname.runes.toList()
+                                      : getTranslated(context, 'empty'))),
+                            ),
+                            ListTile(
+                              title:
+                                  Text(getTranslated(context, 'dateOfBirth')),
+                              subtitle: Text(employee.dateOfBirth != null
+                                  ? employee.dateOfBirth
+                                  : getTranslated(context, 'empty')),
+                            ),
+                            ListTile(
+                              title: Text(getTranslated(context, 'fatherName')),
+                              subtitle: Text(utf8.decode(
+                                  employee.fatherName != null
+                                      ? employee.fatherName.runes.toList()
+                                      : getTranslated(context, 'empty'))),
+                            ),
+                            ListTile(
+                              title: Text(getTranslated(context, 'motherName')),
+                              subtitle: Text(utf8.decode(
+                                  employee.motherName != null
+                                      ? employee.motherName.runes.toList()
+                                      : getTranslated(context, 'empty'))),
+                            ),
+                            ListTile(
+                              title: Text(getTranslated(
+                                  context, 'expirationDateOfWorkInPoland')),
                               subtitle: Text(
-                                  utf8.decode(employee.surname.runes.toList())),
+                                  employee.expirationDateOfWorkInPoland != null
+                                      ? employee.expirationDateOfWorkInPoland
+                                      : getTranslated(context, 'empty')),
                             ),
                             ListTile(
-                              title: Text('Data urodzenia'),
-                              subtitle: Text(employee.dateOfBirth),
+                              title: Text(getTranslated(context, 'nip')),
+                              subtitle: Text(employee.nip != null
+                                  ? employee.nip
+                                  : getTranslated(context, 'empty')),
                             ),
                             ListTile(
-                              title: Text('Imię ojca'),
-                              subtitle: Text(utf8
-                                  .decode(employee.fatherName.runes.toList())),
+                              title: Text(
+                                  getTranslated(context, 'bankAccountNumber')),
+                              subtitle: Text(employee.bankAccountNumber != null
+                                  ? employee.bankAccountNumber
+                                  : getTranslated(context, 'empty')),
                             ),
                             ListTile(
-                              title: Text('Imię matki'),
-                              subtitle: Text(utf8
-                                  .decode(employee.motherName.runes.toList())),
+                              title:
+                                  Text(getTranslated(context, 'moneyPerHour')),
+                              subtitle: Text(employee.moneyPerHour != null
+                                  ? employee.moneyPerHour.toString()
+                                  : getTranslated(context, 'empty')),
                             ),
                             ListTile(
-                              title: Text('Data ważności pracy w Polsce'),
-                              subtitle:
-                                  Text(employee.expirationDateOfWorkInPoland),
+                              title: Text(
+                                  getTranslated(context, 'drivingLicense')),
+                              subtitle: Text(employee.drivingLicense != null
+                                  ? employee.drivingLicense
+                                  : getTranslated(context, 'empty')),
                             ),
                             ListTile(
-                              title: Text('NIP'),
-                              subtitle: Text(employee.nip),
+                              title:
+                                  Text(getTranslated(context, 'houseNumber')),
+                              subtitle: Text(employee.houseNumber != null
+                                  ? employee.houseNumber.toString()
+                                  : getTranslated(context, 'empty')),
                             ),
                             ListTile(
-                              title: Text('Number konta bankowego'),
-                              subtitle: Text(employee.bankAccountNumber),
+                              title: Text(getTranslated(context, 'street')),
+                              subtitle: Text(employee.street != null
+                                  ? utf8.decode(employee.street.runes.toList())
+                                  : getTranslated(context, 'empty')),
                             ),
                             ListTile(
-                              title: Text('Stawka godzinowa'),
-                              subtitle: Text(employee.moneyPerHour.toString()),
+                              title: Text(getTranslated(context, 'zipCode')),
+                              subtitle: Text(employee.zipCode != null
+                                  ? employee.zipCode
+                                  : getTranslated(context, 'empty')),
                             ),
                             ListTile(
-                              title: Text('Prawo jazdy'),
-                              subtitle: Text(employee.drivingLicense),
+                              title: Text(getTranslated(context, 'locality')),
+                              subtitle: Text(employee.locality != null
+                                  ? utf8
+                                      .decode(employee.locality.runes.toList())
+                                  : getTranslated(context, 'empty')),
                             ),
                             ListTile(
-                              title: Text('Numer domu'),
-                              subtitle: Text(employee.houseNumber.toString()),
+                              title: Text(
+                                  getTranslated(context, 'passportNumber')),
+                              subtitle: Text(employee.passportNumber != null
+                                  ? employee.passportNumber
+                                  : getTranslated(context, 'empty')),
                             ),
                             ListTile(
-                              title: Text('Ulica'),
+                              title: Text(getTranslated(
+                                  context, 'passportReleaseDate')),
                               subtitle: Text(
-                                  utf8.decode(employee.street.runes.toList())),
+                                  employee.passportReleaseDate != null
+                                      ? employee.passportReleaseDate
+                                      : getTranslated(context, 'empty')),
                             ),
                             ListTile(
-                              title: Text('Kod pocztowy'),
-                              subtitle: Text(employee.zipCode),
+                              title: Text(getTranslated(
+                                  context, 'passportExpirationDate')),
+                              subtitle: Text(
+                                  employee.passportExpirationDate != null
+                                      ? employee.passportExpirationDate
+                                      : getTranslated(context, 'empty')),
                             ),
                             ListTile(
-                              title: Text('Miejscowość'),
-                              subtitle: Text(utf8
-                                  .decode(employee.locality.runes.toList())),
+                              title: Text(getTranslated(context, 'groupId')),
+                              subtitle: Text(employee.groupId != null
+                                  ? employee.groupId.toString()
+                                  : getTranslated(context, 'empty')),
                             ),
                             ListTile(
-                              title: Text('Numer paszportu'),
-                              subtitle: Text(employee.passportNumber),
-                            ),
-                            ListTile(
-                              title: Text('Data wydania paszportu'),
-                              subtitle: Text(employee.passportReleaseDate),
-                            ),
-                            ListTile(
-                              title: Text('Data ważności paszportu'),
-                              subtitle: Text(employee.passportExpirationDate),
-                            ),
-                            ListTile(
-                              title: Text('Identyfikator grupy'),
-                              subtitle: Text(employee.groupId.toString()),
-                            ),
-                            ListTile(
-                              title: Text('Nazwa grupy'),
-                              subtitle: Text(utf8
-                                  .decode(employee.groupName.runes.toList())),
+                              title: Text(getTranslated(context, 'groupName')),
+                              subtitle: Text(employee.groupName != null &&
+                                      employee.groupName != 'Brak'
+                                  ? utf8
+                                      .decode(employee.groupName.runes.toList())
+                                  : getTranslated(context, 'empty')),
                             ),
                           ],
                         ),
