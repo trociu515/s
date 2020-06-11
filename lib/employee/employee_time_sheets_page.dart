71ffa3507d9712234d3eff9def912589a71dc0d4
RobertKrzywina <robert.krzywina@gmail.com>
2020-06-11 15:35:42 +0200

    Add translate month method with json without translations

--- lib/employee/employee_time_sheets_page.dart
+++ lib/employee/employee_time_sheets_page.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:give_job/employee/employee_side_bar.dart';
 import 'package:give_job/employee/service/employee_time_sheet_service.dart';
 import 'package:give_job/internationalization/localization/localization_constants.dart';
@@ -25,6 +26,36 @@ class EmployeeTimeSheetsPage extends StatefulWidget {
 class _EmployeeTimeSheetsPageState extends State<EmployeeTimeSheetsPage> {
   final EmployeeTimeSheetService _employeeService =
       new EmployeeTimeSheetService();
+
+  String translateMonth(String toTranslate) {
+    switch (toTranslate) {
+      case JANUARY:
+        return getTranslated(context, 'january');
+      case FEBRUARY:
+        return getTranslated(context, 'february');
+      case MARCH:
+        return getTranslated(context, 'march');
+      case APRIL:
+        return getTranslated(context, 'april');
+      case MAY:
+        return getTranslated(context, 'may');
+      case JUNE:
+        return getTranslated(context, 'june');
+      case JULY:
+        return getTranslated(context, 'july');
+      case AUGUST:
+        return getTranslated(context, 'august');
+      case SEPTEMBER:
+        return getTranslated(context, 'september');
+      case OCTOBER:
+        return getTranslated(context, 'october');
+      case NOVEMBER:
+        return getTranslated(context, 'november');
+      case DECEMBER:
+        return getTranslated(context, 'december');
+    }
+    throw 'Wrong month to translate!';
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -63,35 +94,6 @@ class _EmployeeTimeSheetsPageState extends State<EmployeeTimeSheetsPage> {
                             crossAxisAlignment: CrossAxisAlignment.center,
                             children: <Widget>[
                               ListTile(
-                                title: Text(timeSheet.year.toString() +
-                                    ' ' +
-                                    timeSheet.month +
-                                    '\n' +
-                                    utf8.decode(
-                                        timeSheet.groupName.runes.toList())),
-                                subtitle: Wrap(
-                                  children: <Widget>[
-                                    Text('Przepracowany czas: ' +
-                                        timeSheet.totalHours.toString() +
-                                        'h'),
-                                    Text('Średnia ocena: ' +
-                                        timeSheet.averageEmployeeRating
-                                            .toString()),
-                                  ],
-                                ),
-                                trailing: Wrap(
-                                  children: <Widget>[
-                                    Text(
-                                      '+ ' +
-                                          timeSheet.totalMoneyEarned.toString(),
-                                      style: TextStyle(color: Colors.green),
-                                    ),
-                                    Icon(
-                                      Icons.attach_money,
-                                      color: Colors.green,
-                                    ),
-                                  ],
-                                ),
                                 leading: Icon(
                                   timeSheet.status == 'Accepted'
                                       ? Icons.check_circle_outline
@@ -99,6 +101,38 @@ class _EmployeeTimeSheetsPageState extends State<EmployeeTimeSheetsPage> {
                                   color: timeSheet.status == 'Accepted'
                                       ? Colors.green
                                       : Colors.orange,
+                                ),
+                                title: Text(timeSheet.year.toString() +
+                                    ' ' +
+                                    translateMonth(timeSheet.month) +
+                                    '\n' +
+                                    utf8.decode(
+                                        timeSheet.groupName.runes.toList())),
+                                subtitle: Wrap(
+                                  children: <Widget>[
+                                    Text(getTranslated(context, 'hoursWorked') +
+                                        ': ' +
+                                        timeSheet.totalHours.toString() +
+                                        'h'),
+                                    Text(getTranslated(
+                                            context, 'averageRating') +
+                                        ': ' +
+                                        timeSheet.averageEmployeeRating
+                                            .toString()),
+                                  ],
+                                ),
+                                trailing: Wrap(
+                                  children: <Widget>[
+                                    Text(
+                                      timeSheet.totalMoneyEarned.toString(),
+                                      style: TextStyle(
+                                          color: Colors.green, fontSize: 20),
+                                    ),
+                                    Icon(
+                                      Icons.attach_money,
+                                      color: Colors.green,
+                                    ),
+                                  ],
                                 ),
                               ),
                             ],
--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -4,3 +4,16 @@ const String APP_NAME = 'Give Job';
 
 const String ROLE_EMPLOYEE = 'ROLE_EMPLOYEE';
 const String ROLE_MANAGER = 'ROLE_MANAGER';
+
+const String JANUARY = "January";
+const String FEBRUARY = "February";
+const String MARCH = "March";
+const String APRIL = "April";
+const String MAY = "May";
+const String JUNE = "June";
+const String JULY = "July";
+const String AUGUST = "August";
+const String SEPTEMBER = "September";
+const String OCTOBER = "October";
+const String NOVEMBER = "November";
+const String DECEMBER = "December";
