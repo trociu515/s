57b1ba00d29623cb321645d37175188c60eca70b
RobertKrzywina <robert.krzywina@gmail.com>
2020-08-13 00:27:02 +0200

    Improve design of get started page

--- lib/get_started_page.dart
+++ lib/get_started_page.dart
@@ -30,9 +30,7 @@ class _GetStartedPageState extends State<GetStartedPage> {
     for (Language language in languages) {
       items.add(
         DropdownMenuItem(
-          value: language,
-          child: Text(language.name + ' ' + language.flag),
-        ),
+            value: language, child: Text(language.name + ' ' + language.flag)),
       );
     }
     return items;
@@ -55,114 +53,94 @@ class _GetStartedPageState extends State<GetStartedPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Image.asset(
-              'images/logo.png',
-              height: 125,
-            ),
-            SizedBox(
-              height: 20,
-            ),
+            Image.asset('images/logo.png', height: 125),
+            SizedBox(height: 20),
             Text(
               getTranslated(context, 'getStartedTitle'),
               textAlign: TextAlign.center,
               style: TextStyle(color: Colors.white, fontSize: 30),
             ),
-            SizedBox(height: 10),
+            SizedBox(height: 30),
             Container(
               child: Text(
                 getTranslated(context, 'getStartedDescription'),
                 textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.white, fontSize: 17),
+                style: TextStyle(color: Colors.white, fontSize: 19),
               ),
             ),
-            SizedBox(
-              height: 20,
-            ),
+            SizedBox(height: 30),
             Center(
               child: Text(getTranslated(context, 'getStartedLanguage'),
                   textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.white, fontSize: 17)),
+                  style: TextStyle(color: Colors.white, fontSize: 19)),
             ),
-            SizedBox(
-              height: 10,
-            ),
+            SizedBox(height: 5),
             Container(
               child: Center(
                 child: Theme(
-                  data: Theme.of(context).copyWith(
-                    canvasColor: DARK,
-                  ),
+                  data: Theme.of(context).copyWith(canvasColor: DARK),
                   child: Column(
                     children: <Widget>[
                       DropdownButtonHideUnderline(
-                        child: DropdownButton(
-                          style: TextStyle(color: Colors.white, fontSize: 37),
-                          value: _selectedLanguage,
-                          items: _dropdownMenuItems,
-                          onChanged: (Language language) {
-                            _changeLanguage(language, context);
-                          },
-                        ),
-                      ),
-                      SizedBox(
-                        height: 35,
-                      ),
+                          child: DropdownButton(
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 25),
+                              value: _selectedLanguage,
+                              items: _dropdownMenuItems,
+                              onChanged: (Language language) =>
+                                  (_changeLanguage(language, context)))),
+                      SizedBox(height: 20),
                     ],
                   ),
                 ),
               ),
             ),
             MaterialButton(
-              elevation: 0,
-              height: 50,
-              shape: new RoundedRectangleBorder(
-                borderRadius: new BorderRadius.circular(30.0),
-              ),
-              onPressed: () {
-                storage.write(key: 'getStartedClick', value: 'click');
-                Navigator.push(
-                  context,
-                  PageRouteBuilder(
-                    pageBuilder: (BuildContext context,
-                        Animation<double> animation,
-                        Animation<double> secondaryAnimation) {
-                      return LoginPage();
-                    },
-                    transitionsBuilder: (BuildContext context,
-                        Animation<double> animation,
-                        Animation<double> secondaryAnimation,
-                        Widget child) {
-                      return SlideTransition(
-                        position: new Tween<Offset>(
-                          begin: const Offset(-1.0, 0.0),
-                          end: Offset.zero,
-                        ).animate(animation),
-                        child: new SlideTransition(
+                elevation: 0,
+                height: 50,
+                shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(30.0)),
+                onPressed: () {
+                  storage.write(key: 'getStartedClick', value: 'click');
+                  Navigator.push(
+                    context,
+                    PageRouteBuilder(
+                      pageBuilder: (BuildContext context,
+                          Animation<double> animation,
+                          Animation<double> secondaryAnimation) {
+                        return LoginPage();
+                      },
+                      transitionsBuilder: (BuildContext context,
+                          Animation<double> animation,
+                          Animation<double> secondaryAnimation,
+                          Widget child) {
+                        return SlideTransition(
                           position: new Tween<Offset>(
-                            begin: Offset.zero,
-                            end: const Offset(-1.0, 0.0),
-                          ).animate(secondaryAnimation),
-                          child: child,
-                        ),
-                      );
-                    },
-                  ),
-                );
-              },
-              color: GREEN,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(getTranslated(context, 'getStarted'),
-                      style: TextStyle(color: Colors.white, fontSize: 20)),
-                  Icon(Icons.arrow_forward_ios)
-                ],
-              ),
-              textColor: Colors.white,
-            ),
-            SizedBox(
-              height: 30,
-            ),
+                            begin: const Offset(-1.0, 0.0),
+                            end: Offset.zero,
+                          ).animate(animation),
+                          child: new SlideTransition(
+                              position: new Tween<Offset>(
+                                begin: Offset.zero,
+                                end: const Offset(-1.0, 0.0),
+                              ).animate(secondaryAnimation),
+                              child: child),
+                        );
+                      },
+                    ),
+                  );
+                },
+                color: GREEN,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(getTranslated(context, 'getStarted'),
+                        style: TextStyle(color: Colors.white, fontSize: 20)),
+                    Icon(Icons.arrow_forward_ios)
+                  ],
+                ),
+                textColor: Colors.white),
+            SizedBox(height: 30),
           ],
         ),
       ),
