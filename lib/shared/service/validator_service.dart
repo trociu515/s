82220b0fde7b7f0035f0f188615b8900ddc9b8c6
RobertKrzywina <robert.krzywina@gmail.com>
2020-08-16 16:35:09 +0200

    Little refactor of validator service

--- lib/shared/service/validator_service.dart
+++ lib/shared/service/validator_service.dart
@@ -5,35 +5,32 @@ import 'package:give_job/internationalization/localization/localization_constant
 class ValidatorService {
   static String validateLoginCredentials(
       String username, String password, BuildContext context) {
-    String invalidMessage;
     if (username.isEmpty && password.isEmpty) {
-      invalidMessage = getTranslated(context, 'usernameAndPasswordRequired');
+      return getTranslated(context, 'usernameAndPasswordRequired');
     } else if (username.isEmpty) {
-      invalidMessage = getTranslated(context, 'usernameRequired');
+      return getTranslated(context, 'usernameRequired');
     } else if (password.isEmpty) {
-      invalidMessage = getTranslated(context, 'passwordRequired');
+      return getTranslated(context, 'passwordRequired');
     }
-    return invalidMessage;
+    return null;
   }
 
   static String validateUpdatingHours(int hours, BuildContext context) {
-    String invalidMessage;
     if (hours.isNegative) {
-      invalidMessage = getTranslated(context, 'hoursCannotBeLowerThan0');
+      return getTranslated(context, 'hoursCannotBeLowerThan0');
     } else if (hours > 24) {
-      invalidMessage = getTranslated(context, 'hoursCannotBeHigherThan24');
+      return getTranslated(context, 'hoursCannotBeHigherThan24');
     }
-    return invalidMessage;
+    return null;
   }
 
   static String validateUpdatingRating(int rating, BuildContext context) {
-    String invalidMessage;
     if (rating < 1) {
-      invalidMessage = getTranslated(context, 'ratingCannotBeLowerThan1');
+      return getTranslated(context, 'ratingCannotBeLowerThan1');
     } else if (rating > 10) {
-      invalidMessage = getTranslated(context, 'ratingCannotBeHigherThan10');
+      return getTranslated(context, 'ratingCannotBeHigherThan10');
     }
-    return invalidMessage;
+    return null;
   }
 
   static String validateUpdatingComment(String comment, BuildContext context) {
