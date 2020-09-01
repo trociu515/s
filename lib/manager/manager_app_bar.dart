c9088bf4faaaa09e2854312eea15d0cff5caf165
RobertKrzywina <robert.krzywina@gmail.com>
2020-09-01 16:14:04 +0200

    Change colors of manager app bar

--- lib/manager/manager_app_bar.dart
+++ lib/manager/manager_app_bar.dart
@@ -4,16 +4,21 @@ import 'package:give_job/manager/informations/manager_information_page.dart';
 import 'package:give_job/shared/libraries/colors.dart';
 import 'package:give_job/shared/model/user.dart';
 import 'package:give_job/shared/settings/settings_page.dart';
+import 'package:give_job/shared/widget/icons.dart';
 
 AppBar managerAppBar(BuildContext context, User user, String title) {
   return AppBar(
+    iconTheme: IconThemeData(color: WHITE),
+    backgroundColor: BRIGHTER_DARK,
+    elevation: 0.0,
+    bottomOpacity: 0.0,
     title: Text(
       title,
-      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15, color: DARK),
+      style: TextStyle(fontSize: 15, color: WHITE),
     ),
     actions: <Widget>[
       IconButton(
-        icon: Icon(Icons.person),
+        icon: iconWhite(Icons.person),
         onPressed: () {
           Navigator.push(
             context,
@@ -25,7 +30,7 @@ AppBar managerAppBar(BuildContext context, User user, String title) {
       Padding(
         padding: EdgeInsets.only(right: 15.0),
         child: IconButton(
-          icon: Icon(Icons.settings),
+          icon: iconWhite(Icons.settings),
           onPressed: () {
             Navigator.push(
               context,
