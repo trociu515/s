4b589dbfffc6cec5510c9f87cdaa8a9c9c8faae6
RobertKrzywina <robert.krzywina@gmail.com>
2020-09-01 22:38:42 +0200

    Modify employee profile in manager page

--- lib/manager/groups/group/employee/manager_employee_time_sheets_page.dart
+++ lib/manager/groups/group/employee/manager_employee_time_sheets_page.dart
@@ -96,114 +96,135 @@ class _ManagerEmployeeTimeSheetsPageState
               appBar: managerAppBar(
                   context,
                   _model.user,
-                  getTranslated(context, 'timesheets') +
+                  getTranslated(context, 'employee') +
                       ' - ' +
                       utf8.decode(_model.groupName != null
                           ? _model.groupName.runes.toList()
                           : '-')),
               drawer: managerSideBar(context, _model.user),
               body: SingleChildScrollView(
-                child: Column(
-                  children: <Widget>[
-                    SizedBox(height: 15),
-                    text20WhiteBold(_employeeInfo != null
-                        ? utf8.decode(_employeeInfo.runes.toList()) +
-                            ' ' +
-                            LanguageUtil.findFlagByNationality(
-                                _employeeNationality)
-                        : getTranslated(context, 'empty')),
-                    SizedBox(height: 15),
-                    for (var timeSheet in timeSheets)
-                      Card(
-                        color: BRIGHTER_DARK,
-                        child: InkWell(
-                          onTap: () {
-                            if (timeSheet.status == 'Completed') {
-                              Navigator.of(context).push(
-                                CupertinoPageRoute<Null>(
-                                  builder: (BuildContext context) {
-                                    return ManagerEmployeeTimeSheetsWorkdaysCompletedPage(
-                                        _model,
-                                        _employeeInfo,
-                                        _employeeNationality,
-                                        _currency,
-                                        timeSheet);
-                                  },
-                                ),
-                              );
-                            } else {
-                              Navigator.of(context).push(
-                                CupertinoPageRoute<Null>(
-                                  builder: (BuildContext context) {
-                                    return ManagerEmployeeTimeSheetsWorkdaysInProgressPage(
-                                        _model,
-                                        _employeeInfo,
-                                        _employeeNationality,
-                                        _currency,
-                                        timeSheet);
-                                  },
-                                ),
-                              );
-                            }
-                          },
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: <Widget>[
-                              ListTile(
-                                leading: Padding(
-                                  padding: EdgeInsets.only(bottom: 15),
-                                  child: Image(
-                                    image: timeSheet.status == STATUS_IN_PROGRESS
-                                        ? AssetImage('images/unchecked.png')
-                                        : AssetImage('images/checked.png'),
+                child: Center(
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        width: 100,
+                        height: 100,
+                        margin: EdgeInsets.only(top: 20, bottom: 10),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                              image: AssetImage('images/logo.png'),
+                              fit: BoxFit.fill),
+                        ),
+                      ),
+                      text25White(utf8.decode(_employeeInfo != null
+                          ? _employeeInfo.runes.toList()
+                          : '-')),
+                      SizedBox(height: 2.5),
+                      text20White(LanguageUtil.convertShortNameToFullName(
+                              _employeeNationality) +
+                          ' ' +
+                          LanguageUtil.findFlagByNationality(
+                              _employeeNationality)),
+                      SizedBox(height: 2.5),
+                      text18White(getTranslated(context, 'employee') +
+                          ' #' +
+                          _employeeId.toString()),
+                      SizedBox(height: 10),
+                      for (var timeSheet in timeSheets)
+                        Card(
+                          color: BRIGHTER_DARK,
+                          child: InkWell(
+                            onTap: () {
+                              if (timeSheet.status == 'Completed') {
+                                Navigator.of(context).push(
+                                  CupertinoPageRoute<Null>(
+                                    builder: (BuildContext context) {
+                                      return ManagerEmployeeTimeSheetsWorkdaysCompletedPage(
+                                          _model,
+                                          _employeeInfo,
+                                          _employeeNationality,
+                                          _currency,
+                                          timeSheet);
+                                    },
                                   ),
-                                ),
-                                title: textWhiteBold(timeSheet.year.toString() +
-                                    ' ' +
-                                    MonthUtil.translateMonth(
-                                        context, timeSheet.month)),
-                                subtitle: Column(
-                                  children: <Widget>[
-                                    Align(
+                                );
+                              } else {
+                                Navigator.of(context).push(
+                                  CupertinoPageRoute<Null>(
+                                    builder: (BuildContext context) {
+                                      return ManagerEmployeeTimeSheetsWorkdaysInProgressPage(
+                                          _model,
+                                          _employeeInfo,
+                                          _employeeNationality,
+                                          _currency,
+                                          timeSheet);
+                                    },
+                                  ),
+                                );
+                              }
+                            },
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: <Widget>[
+                                ListTile(
+                                  leading: Padding(
+                                    padding: EdgeInsets.only(bottom: 15),
+                                    child: Image(
+                                      image: timeSheet.status ==
+                                              STATUS_IN_PROGRESS
+                                          ? AssetImage('images/unchecked.png')
+                                          : AssetImage('images/checked.png'),
+                                    ),
+                                  ),
+                                  title: textWhiteBold(
+                                      timeSheet.year.toString() +
+                                          ' ' +
+                                          MonthUtil.translateMonth(
+                                              context, timeSheet.month)),
+                                  subtitle: Column(
+                                    children: <Widget>[
+                                      Align(
+                                          child: Row(
+                                            children: <Widget>[
+                                              textWhite(getTranslated(
+                                                      context, 'hoursWorked') +
+                                                  ': '),
+                                              textGreenBold(timeSheet.totalHours
+                                                      .toString() +
+                                                  'h'),
+                                            ],
+                                          ),
+                                          alignment: Alignment.topLeft),
+                                      Align(
                                         child: Row(
                                           children: <Widget>[
                                             textWhite(getTranslated(
-                                                    context, 'hoursWorked') +
+                                                    context, 'averageRating') +
                                                 ': '),
-                                            textGreenBold(timeSheet.totalHours
-                                                    .toString() +
-                                                'h'),
+                                            textGreenBold(timeSheet
+                                                .averageEmployeeRating
+                                                .toString()),
                                           ],
                                         ),
-                                        alignment: Alignment.topLeft),
-                                    Align(
-                                      child: Row(
-                                        children: <Widget>[
-                                          textWhite(getTranslated(
-                                                  context, 'averageRating') +
-                                              ': '),
-                                          textGreenBold(timeSheet
-                                              .averageEmployeeRating
-                                              .toString()),
-                                        ],
+                                        alignment: Alignment.topLeft,
                                       ),
-                                      alignment: Alignment.topLeft,
-                                    ),
-                                  ],
+                                    ],
+                                  ),
+                                  trailing: Wrap(
+                                    children: <Widget>[
+                                      text20GreenBold(timeSheet.totalMoneyEarned
+                                          .toString()),
+                                      text20GreenBold(' ' + _currency)
+                                    ],
+                                  ),
                                 ),
-                                trailing: Wrap(
-                                  children: <Widget>[
-                                    text20GreenBold(
-                                        timeSheet.totalMoneyEarned.toString()),
-                                    text20GreenBold(' ' + _currency)
-                                  ],
-                                ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
               floatingActionButtonLocation:
